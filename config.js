021e49d08f3d5674d980ca29276f031ac613499b
Nguyễn Thành Long <longnt69@fpt.edu.vn>
2026-06-02 20:42:43 +0700

    Update Supabase key in config.js
    Updated Supabase key in config.js for security reasons.

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // Lưu trữ các thông tin cấu hình nhạy cảm hoặc cấu hình môi trường
 const supabaseUrl = 'https://cideqzumjiwbxjftoarm.supabase.co';
 // WARNING: Điền Anon Key vào thay cho process.env.SUPABASE_KEY vì file HTML chạy trên trình duyệt client
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vYWhhYXViZXB2dnBrdWtqb3VuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYzMjk2NTksImV4cCI6MjA5MTkwNTY1OX0.AgY0-QhMeTXF08eCBRnIxyAjq8mtNHYQBDr6F5jqPcQ';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNpZGVxenVtaml3YnhqZnRvYXJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzOTI2NTMsImV4cCI6MjA5NTk2ODY1M30.u1smKO0PMhiQUd9f_z1Zp6YrenUBbXO0B8IqGpDF6_Q';
